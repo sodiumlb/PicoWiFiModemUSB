6e33adc566e30d798cb5b6988711f0d5d23222be
Sodiumlightbaby <edvard@xox.no>
2024-07-13 08:37:08 +0200

    Split types out from globals header file

--- src/globals.h
+++ src/globals.h
@@ -1,6 +1,6 @@
 #ifndef _GLOBALS_H
    #define _GLOBALS_H
-
+   #include "types.h"
    // globals
    const char okStr[] = {"OK"};
    const char connectStr[] = {"CONNECT"};
@@ -8,61 +8,7 @@
    const char noCarrierStr[] = {"NO CARRIER"};
    const char errorStr[] = {"ERROR"};
    const char noAnswerStr[] = {"NO ANSWER"};
-   enum ResultCodes { R_OK, R_CONNECT, R_RING, R_NO_CARRIER, R_ERROR, R_NO_ANSWER, R_RING_IP };
    const char * const resultCodes[] = { okStr, connectStr, ringStr, noCarrierStr, errorStr, noAnswerStr, ringStr};
-   enum DtrStates { DTR_IGNORE, DTR_GOTO_COMMAND, DTR_END_CALL, DTR_RESET};
-
-   typedef struct Settings {
-      uint16_t      magicNumber;
-      char          ssid[MAX_SSID_LEN + 1];
-      char          wifiPassword[MAX_WIFI_PWD_LEN + 1];
-      uint32_t      serialSpeed;
-      uint8_t       dataBits;
-      uart_parity_t parity;
-      uint8_t       stopBits;
-      bool          rtsCts;
-      uint8_t       width, height;
-      char          escChar;
-      char          alias[SPEED_DIAL_SLOTS][MAX_ALIAS_LEN + 1];
-      char          speedDial[SPEED_DIAL_SLOTS][MAX_SPEED_DIAL_LEN + 1];
-      char          mdnsName[MAX_MDNSNAME_LEN + 1];
-      uint8_t       autoAnswer;
-      uint16_t      listenPort;
-      char          busyMsg[MAX_BUSYMSG_LEN + 1];
-      char          serverPassword[MAX_PWD_LEN + 1];
-      bool          echo;
-      uint8_t       telnet;
-      char          autoExecute[MAX_AUTOEXEC_LEN + 1];
-      char          terminal[MAX_TERMINAL_LEN + 1];
-      char          location[MAX_LOCATION_LEN + 1];
-      bool          startupWait;
-      bool          extendedCodes;
-      bool          verbose;
-      bool          quiet;
-      DtrStates     dtrHandling;
-   } SETTINGS_T;
-   
-   typedef struct TCP_CLIENT_T_ {
-      struct tcp_pcb *pcb;
-      ip_addr_t remoteAddr;
-      volatile bool connected;
-      volatile bool connectFinished;
-      volatile bool waitingForAck;
-      volatile uint8_t rxBuff[TCP_CLIENT_RX_BUF_SIZE];
-      volatile uint16_t rxBuffLen;
-      volatile uint16_t rxBuffHead;
-      volatile uint16_t rxBuffTail;
-      volatile uint16_t totLen;
-      volatile uint8_t txBuff[TCP_CLIENT_TX_BUF_SIZE];
-      volatile uint16_t txBuffLen;
-      volatile uint16_t txBuffHead;
-      volatile uint16_t txBuffTail;
-   } TCP_CLIENT_T;
-   
-   typedef struct TCP_SERVER_T_ {
-      struct tcp_pcb *pcb;
-      struct tcp_pcb *clientPcb;
-   } TCP_SERVER_T;
    
    SETTINGS_T settings;
    TCP_CLIENT_T *tcpClient,tcpClient0,tcpDroppedClient;
