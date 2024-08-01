533e3af8ea971a419c473a97047c4e56be9c31f6
Sodiumlightbaby <edvard@xox.no>
2024-08-01 20:00:02 +0200

    Fix debug define belongs in wifi_modem.h

--- src/wifi_modem.cpp
+++ src/wifi_modem.cpp
@@ -26,7 +26,6 @@
 //   You should have received a copy of the GNU General Public License
 //   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
-#define DEBUG 1
 #include <string.h>
 #include <time.h>
 #include <malloc.h>
